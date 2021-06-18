ebc81bbe2f732c68aac98a3e7a9d1ea848bd36a2
Steven Nguyen <32990646+Tsurugi24@users.noreply.github.com>
2021-06-17 19:09:30 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -85,4 +85,17 @@ function freeze() {
         draw()
     }
 }
+
+function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+    if(!isAtLeftEdge) currentPosition -= 1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken')))
+    currentPosition += 1
+}
+
+draw()
+
 })
