7d285de7db47ecfdb20bcfd656fccc7515eb478b
Steven Nguyen <32990646+Tsurugi24@users.noreply.github.com>
2021-06-21 04:02:43 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const ScoreDisplay = document.querySelector('#score')
     const StartBtn = document.querySelector('#start-button')
     const width = 10
+    let nextRandom = 0
 
     //Tetromino dimensions
 const lTetromino = [
@@ -93,10 +94,12 @@ function freeze() {
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
         current.forEach(index => squares[currentPosition + index].classList.add('taken'))
         //starts a new falling tetromino
-        random = Math.floor(Math.random() * theTetrominoes.length)
+        random = nextRandom
+        nextRandom = Math.floor(Math.random() * theTetrominoes.length)
         current = theTetrominoes[random][currentRotation]
         currentPosition = 4
         draw()
+        displayShape()
     }
 }
 
@@ -129,6 +132,31 @@ function rotate() {
     }
     current = theTetrominoes[random][currentRotation]
     draw()
+}
+
+//shows the next tetromino in the mini-grid
+const displaySquares = document.querySelector('.mini-grid div')
+const displayWidth = 4
+let displayIndex = 0
+
+//Tetrominos without rotations
+const upNextTetrominos = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2] //lTetromino
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1] //zTetromino
+    [1, displayWidth, displayWidth + 1, displayWidth + 2] //tTetromino
+    [0, 1, displayWidth, displayWidth + 1] //oTetromino
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1] //iTetromino
+]
+
+//displays the shape in the mini-grid
+function displayShape() {
+    //removes tetromino from the entire grid
+    displaySquares.forEach(square => {
+        square.classList.remove('tetromino')
+    })
+    upNextTetrominos[nextRandom].forEach( index => {
+        displaySquares[displayIndex + index].classList.add('tetromino')
+    })
 }
 
 })
